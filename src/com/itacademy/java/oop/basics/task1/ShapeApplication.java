03e7dfd58fc2880f192589eec4cc4a0f1d44b223
unknown <mantas.ambrazaitis9@gmail.com>
2022-03-27 00:54:46 +0200

    task1 fix

--- src/com/itacademy/java/oop/basics/task1/ShapeApplication.java
+++ src/com/itacademy/java/oop/basics/task1/ShapeApplication.java
@@ -2,8 +2,8 @@ package com.itacademy.java.oop.basics.task1;
 
 public class ShapeApplication {
     public static void main(String[] args) {
-        Square square = new Square();
-        Circle circle = new Circle();
+        Shape square = new Square();
+        Shape circle = new Circle();
 
         square.calculateArea(10);
         square.calculatePerimeter(10);
